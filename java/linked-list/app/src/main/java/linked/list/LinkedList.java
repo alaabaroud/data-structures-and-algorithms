55054710bedca57e3b43e6a035b724bc48104f11
ALAABAROUD <alaabaroud783@gmail.com>
2021-10-17 21:01:57 +0300

    code challenge 6

--- java/linked-list/app/src/main/java/linked/list/LinkedList.java
+++ java/linked-list/app/src/main/java/linked/list/LinkedList.java
@@ -3,12 +3,12 @@ package linked.list;
 
 public class LinkedList <T>{
     Node<T> head;
-    
+
     public LinkedList(){
         this.head = null;
 
     }
-    
+
     public void insert (T value){
     Node<T> newNode = new Node<T>(value);
     newNode.next = head;
@@ -20,22 +20,80 @@ public class LinkedList <T>{
         while (current != null){
         if (current.value.equals(value)){
              return true;
-    
+
         }
     current = current.next;
     }
     return false;
     }
 
-    
+
     public String toString(){
         Node<T> current = head;
         String result ="";
         while (current != null){
-            result+="{"+current.value+ "} --->";
+            result+=" {"+current.value+ "} --->";
             current =current.next;
         }
-        result+="NULL";
+//         result+="NULL";
         return result;
     }
+
+    public void append(T value){
+        Node<T> newNode = new Node<>(value);
+if (head == null) {
+    head = newNode;
+}else {
+    Node<T> current = head;
+    while (current.next !=null){
+
+        current = current.next;
+    }
+    current.next= newNode;
+}
+}
+
+
+public void insertBefore (T value, T newValue){
+    Node<T> newNode = new Node<>(newValue);
+    if (head== null) {
+        head = newNode;
+
+    }else if (head.value == value){
+        Node<T> rightNode = head;
+        head=newNode;
+        newNode.next = rightNode;
+
+    }else {
+        Node<T> current= head;
+        while (current.next != null) {
+            if (current.next.value == value) {
+                Node<T> righNode= current.next;
+                current.next = newNode;
+                newNode.next = righNode;
+            }
+            current=current.next;
+        }
+    }
+
+
+}
+
+public void insertAfter(T value, int newValue) {
+Node<T> newNode = new Node(newValue);
+if (head == null){
+    head= newNode;
+}else {
+    Node<T> current = head;
+    while (current!= null){
+        if (current.value== value) {
+            Node<T> rightNode = current.next;
+            current.next = newNode;
+            newNode.next = rightNode;
+        }
+        current = current.next;
+    }
+}
+}
+
 }
