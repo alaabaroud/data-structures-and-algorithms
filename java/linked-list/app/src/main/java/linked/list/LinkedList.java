141faecf923ad0c0dca2eb5fe806f5a321589ef0
ALAABAROUD <alaabaroud783@gmail.com>
2021-10-19 15:16:14 +0300

    codechallenge7

--- java/linked-list/app/src/main/java/linked/list/LinkedList.java
+++ java/linked-list/app/src/main/java/linked/list/LinkedList.java
@@ -1,6 +1,8 @@
 package linked.list;
 
 
+import java.util.*;
+
 public class LinkedList <T>{
     Node<T> head;
 
@@ -32,14 +34,96 @@ public class LinkedList <T>{
         Node<T> current = head;
         String result ="";
         while (current != null){
-            result+="{"+current.value+ "} --->";
+            result+=" {"+current.value+ "} --->";
             current =current.next;
         }
-        result+="NULL";
+//         result+="NULL";
         return result;
     }
 
-//    public String kth (int k){
-//
-//    }
+    public void append(T value){
+        Node<T> newNode = new Node<>(value);
+if (head == null) {
+    head = newNode;
+}else {
+    Node<T> current = head;
+    while (current.next !=null){
+
+        current = current.next;
+    }
+    current.next= newNode;
+}
+}
+
+
+public void insertBefore (T value, T newValue){
+    Node<T> newNode = new Node<>(newValue);
+    if (head== null) {
+        head = newNode;
+
+    }else if (head.value == value){
+        Node<T> rightNode = head;
+        head=newNode;
+        newNode.next = rightNode;
+
+    }else {
+        Node<T> current= head;
+        while (current.next != null) {
+            if (current.next.value == value) {
+                Node<T> righNode= current.next;
+                current.next = newNode;
+                newNode.next = righNode;
+            }
+            current=current.next;
+        }
+    }
+
+
+}
+
+public void insertAfter(T value, int newValue) {
+Node<T> newNode = new Node(newValue);
+if (head == null){
+    head= newNode;
+
+}else {
+    Node<T> current = head;
+    while (current!= null){
+        if (current.value== value) {
+            Node<T> rightNode = current.next;
+            current.next = newNode;
+            newNode.next = rightNode;
+        }
+        current = current.next;
+    }
+}
+
+}
+
+public String kth (int K) {
+    Node <T> current = head;
+    int counter =0;
+    while(current.next != null) {
+        current = current.next;
+        counter++;
+    }
+if ( K > counter || K <0){
+    return "error";
+
+}else{
+    current =head;
+    for (int i = 0; i <= counter-K; i++) {
+        if (i== counter - K) {
+            return  ""+current.value+"" ;
+        }
+        current = current.next;
+    }
+}
+return "";
+
+}
+
+
+
+
 }
--- java/linked-list/app/src/test/java/linked/list/AppTest.java
+++ java/linked-list/app/src/test/java/linked/list/AppTest.java
@@ -3,12 +3,34 @@
  */
 package linked.list;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+public class AppTest {
+
+
+  @DisplayName("this tests for the insertions ")
+  @Test
+  public void insertionsTests() {
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    linkedList.append(1);
+    linkedList.append(80);
+    linkedList.append(3);
+    System.out.println(linkedList);
+    Assertions.assertEquals( " {1} ---> {80} ---> {3} --->", linkedList.toString() );
+
+    linkedList.insertBefore(80, 5);
+    Assertions.assertEquals( " {1} ---> {5} ---> {80} ---> {3} --->", linkedList.toString() );
+
+    linkedList.insertAfter(80, 5);
+    Assertions.assertEquals( " {1} ---> {5} ---> {80} ---> {5} ---> {3} --->", linkedList.toString() );
+    linkedList.kth(0);
+    Assertions.assertEquals("1", linkedList.toString());
+
+
+
+  }
+
+
 }
